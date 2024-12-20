cc6f538c1dbae79c2700a7cb06fcbbfe8a658220
5anne <tahaminaanne@gmail.com>
2024-12-20 20:11:28 +0600

    item_selection_done

--- scripts/add-delete-item.js
+++ scripts/add-delete-item.js
@@ -1,19 +1,66 @@
+const sizeButtons = document.querySelectorAll('button[id^="S"], button[id^="M"], button[id^="L"], button[id^="XL"]');
+const colorButtons = document.querySelectorAll('button[id^="purple"], button[id^="cyan"], button[id^="blue"], button[id^="black"]');
+
 let quantity = 0;
+let selectedColor = 'purple';
+let selectedSize = 'S';
+let itemsQuantity = [
+    {
+        purple: { S: 0, M: 0, L: 0, XL: 0 },
+        cyan: { S: 0, M: 0, L: 0, XL: 0 },
+        blue: { S: 0, M: 0, L: 0, XL: 0 },
+        black: { S: 0, M: 0, L: 0, XL: 0 },
+    }
+]
+
+colorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const colorValue = button.innerText;
+        if (colorValue === 'p') {
+            selectedColor = 'purple';
+            document.getElementById('itemNumber').textContent = itemsQuantity[0][selectedColor][selectedSize];
+            document.getElementById('checkOutItem').textContent = quantity;
+        } else if (colorValue === 'c') {
+            selectedColor = 'cyan';
+            document.getElementById('itemNumber').textContent = itemsQuantity[0][selectedColor][selectedSize];
+            document.getElementById('checkOutItem').textContent = quantity;
+        } else if (colorValue === 'b') {
+            selectedColor = 'blue';
+            document.getElementById('itemNumber').textContent = itemsQuantity[0][selectedColor][selectedSize];
+            document.getElementById('checkOutItem').textContent = quantity;
+        } else if (colorValue === 'k') {
+            selectedColor = 'black';
+            document.getElementById('itemNumber').textContent = itemsQuantity[0][selectedColor][selectedSize];
+            document.getElementById('checkOutItem').textContent = quantity;
+        }
+    })
+})
+
+sizeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        selectedSize = button.textContent.split(' ')[0];
+        document.getElementById('itemNumber').textContent = itemsQuantity[0][selectedColor][selectedSize];
+        document.getElementById('checkOutItem').textContent = quantity;
+    });
+});
 
 function handleAddItem() {
+    itemsQuantity[0][selectedColor][selectedSize]++;
     quantity++;
-    if (quantity > 0) {
-        document.getElementById('itemNumber').textContent = quantity;
-        document.getElementById('checkOutItem').textContent = quantity;
-    }
+    document.getElementById('itemNumber').textContent = itemsQuantity[0][selectedColor][selectedSize];
+    document.getElementById('checkOutItem').textContent = quantity;
 }
 
 function handleDeleteItem() {
-    if (quantity > 0) {
-        quantity--;
+    if (itemsQuantity[0][selectedColor][selectedSize] > 0) {
+        itemsQuantity[0][selectedColor][selectedSize]--;
+        if (quantity > 0) {
+            quantity--;
+        }
+        console.log(quantity);
+        document.getElementById('itemNumber').textContent = itemsQuantity[0][selectedColor][selectedSize];
+        document.getElementById('checkOutItem').textContent = quantity;
     }
-    document.getElementById('itemNumber').textContent = quantity;
-    document.getElementById('checkOutItem').textContent = quantity;
     if (quantity === 0) {
         document.getElementById('checkOut').classList.add('hidden');
     }
